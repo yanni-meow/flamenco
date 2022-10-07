dcb7d5a599278ced37ede4ba064dfcbae03e1c2d
Yanina <yamamporiya@wallarm.com>
2022-10-07 23:45:35 +0400

    exit intent pop-up added

--- script/menu.js
+++ script/menu.js
@@ -1,17 +1,19 @@
 const openBtn = document.querySelector('.header_open_menu');
-const closeBtn = document.querySelector('.menu_close');
 const menu = document.querySelector('.menu_box');
 const items = document.querySelectorAll('.menu_list_item');
+const blur = document.querySelector('.blur');
 
 menu.addEventListener('wheel', (event) => event.preventDefault());
 menu.addEventListener('touchmove', (event) => event.preventDefault());
 
 openBtn.addEventListener('click', function () {
-    menu.classList.remove('hidden')
+    menu.classList.remove('hidden');
+    blur.classList.remove('hidden');
 })
 
-const handleClose = () => menu.classList.add('hidden')
-closeBtn.addEventListener('click', handleClose)
 items.forEach(el => {
-    el.addEventListener('click', handleClose)
+    el.addEventListener('click', () => {
+        menu.classList.add('hidden')
+        document.querySelector('.blur').classList.add('hidden')
+    });
 })
---
+++ script/modals.js
@@ -0,0 +1,65 @@
+const modals = document.querySelectorAll('.pop-up');
+
+// modal windows can't be scrolled
+modals.forEach(modal => {
+    modal.addEventListener('wheel', (event) => event.preventDefault());
+    modal.addEventListener('touchmove', (event) => event.preventDefault());
+});
+
+// modal windows closes with data-value of close btn
+const closeBtns = document.querySelectorAll('.pop-up_close');
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        modals.forEach(modal => {
+            if (modal.classList.contains(btn.dataset.value) || modal.classList.contains('blur')) modal.classList.add('hidden')
+        })
+    })
+});
+
+// to open pop up when client going to leave website
+const showExitIntentPopUp = () => {
+    document.querySelector('.exit_intent').classList.remove('hidden');
+    document.querySelector('.blur').classList.remove('hidden');
+};
+
+// if desktop
+const mouseEvent = e => {
+    if (!e.toElement && !e.relatedTarget) {
+        document.removeEventListener('mouseout', mouseEvent);
+        showExitIntentPopUp();
+    }
+};
+
+document.addEventListener('mouseout', mouseEvent);
+
+// browser 'back' button
+const onHistoryBack = () => {
+    if (window.history.state === 'forward') {
+        window.removeEventListener('popstate', onHistoryBack);
+        showExitIntentPopUp();
+    }
+};
+
+if (window.history && window.history.pushState) {
+    window.history.pushState('forward', null, './');
+    window.addEventListener('popstate', onHistoryBack);
+};
+
+// scroll to top
+const negativeScrollHeight = document.body.scrollHeight / 20;
+let lastScrollTop = 0,
+    maxScrollTop = 0;
+
+const scrollEvent = () => {
+    const { scrollTop } = document.documentElement;
+
+    if (scrollTop > lastScrollTop && scrollTop > maxScrollTop) maxScrollTop = scrollTop;
+    if ((maxScrollTop - scrollTop) > negativeScrollHeight) {
+        document.removeEventListener('scroll', scrollEvent);
+        showExitIntentPopUp();
+    };
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+};
+
+document.addEventListener('scroll', scrollEvent);
